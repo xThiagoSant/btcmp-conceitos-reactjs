6761213615c35263d4ba26d2df0a28fc9776e943
Thiago Santanna da Silva <uzmaki_thiago@outlook.com>
2020-10-29 06:26:28 -0300

    resolucao do desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then( response => {      
+      setProjects(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const {data} = await api.post('/repositories', {
+      title:`Project ${Date.now()}`,
+      url:"https://github.com/xThiagoSant/btcmp-conceitos-reactjs"
+    })
+    setProjects([...projects, data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    /*
+    ATENÇÃO THIAGO!!!!!!!!!!
+    Eu tinha feito "CERTO", até que funciona MASSSSSS, aprendi nesta lição que, não devo pensar como desktop, não estou numa rede local,
+    estou numa internet, onde um GET custa caro! então minha primeira solução se fosse um projeto grande
+    custaria ao trazer novamente uma nova lista só por que apaguei uma delas.
+
+    A solução do diego foi melhor, ele já tiha a lista e filtrou tudo removendo apenas o ID que já tinha sido
+    passado na chamada da função! não sendo necessário uma nova chamada a API. 
+
+    const {status} = await api.delete(`/repositories/${id}`)
+    if(status === 204) {
+      const {data} = await api.get('/repositories')
+      setProjects(data)
+    }
+    */
+
+   api.delete(`/repositories/${id}`)
+   setProjects(projects.filter(project => project.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      {projects.map( project => (
+          <li key={project.id}>
+            {project.title}
+            <button onClick={() => handleRemoveRepository(project.id)}> Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
